1e8c51529d344a99ea03968d9ff5b2ccc1823ddd
LorenzovandenDungen <90601094+LorenzovandenDungen@users.noreply.github.com>
2023-06-12 13:13:55 +0200

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav className="main-header navbar navbar-expand navbar-white navbar-light">
+      {/* Left navbar links */}
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/about">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/contact">Contact</a>
+        </li>
+      </ul>
+
+      {/* Right navbar links */}
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <a className="nav-link" href="/login">Login</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/register">Register</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
